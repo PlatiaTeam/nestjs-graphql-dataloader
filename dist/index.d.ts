466e021eac5d650f70316ea43e78955076c2fda1
Christian Holm <cho@cubitech.dk>
2021-11-23 12:59:48 +0100

    New build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,29 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
-import { ModuleRef } from "@nestjs/core";
+import { CallHandler, ExecutionContext, NestInterceptor, Type } from "@nestjs/common";
+import { ContextId, ModuleRef } from "@nestjs/core";
 import DataLoader from "dataloader";
 import { Observable } from "rxjs";
 export interface NestDataLoader<ID, Type> {
     generateDataLoader(): DataLoader<ID, Type>;
 }
+interface DataLoaderFactory {
+    (contextId: ContextId, type: Type<NestDataLoader<any, any>>): Promise<DataLoader<any, any>>;
+}
+export declare class NestDataLoaderContext {
+    private readonly dataloaderFactory;
+    private readonly id;
+    private readonly cache;
+    constructor(dataloaderFactory: DataLoaderFactory);
+    clearAll(): Promise<void>;
+    getLoader(type: Type<NestDataLoader<any, any>>): Promise<DataLoader<any, any>>;
+}
 export declare class DataLoaderInterceptor implements NestInterceptor {
     private readonly moduleRef;
     constructor(moduleRef: ModuleRef);
     intercept(context: ExecutionContext, next: CallHandler): Observable<any>;
+    private createDataLoader;
 }
-export declare const Loader: (...dataOrPipes: (Function | import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>>)[]) => ParameterDecorator;
+export declare const Loader: (...dataOrPipes: (import("@nestjs/common").PipeTransform<any, any> | Type<import("@nestjs/common").PipeTransform<any, any>> | Type<NestDataLoader<any, any>>)[]) => ParameterDecorator;
+export declare const LoaderContext: (...dataOrPipes: any[]) => ParameterDecorator;
 export declare const ensureOrder: (options: any) => any;
 interface IOrderedNestDataLoaderOptions<ID, Type> {
     propertyKey?: string;
--- dist/index.js
+++ dist/index.js
@@ -21,12 +21,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderedNestDataLoader = exports.ensureOrder = exports.Loader = exports.DataLoaderInterceptor = void 0;
+exports.OrderedNestDataLoader = exports.ensureOrder = exports.LoaderContext = exports.Loader = exports.DataLoaderInterceptor = exports.NestDataLoaderContext = void 0;
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const graphql_1 = require("@nestjs/graphql");
 const dataloader_1 = __importDefault(require("dataloader"));
 const NEST_LOADER_CONTEXT_KEY = "NEST_LOADER_CONTEXT_KEY";
+class NestDataLoaderContext {
+    constructor(dataloaderFactory) {
+        this.dataloaderFactory = dataloaderFactory;
+        this.id = core_1.ContextIdFactory.create();
+        this.cache = new Map();
+    }
+    clearAll() {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (const loaderPromise of this.cache.values()) {
+                const loader = yield loaderPromise;
+                loader.clearAll();
+            }
+        });
+    }
+    getLoader(type) {
+        let loader = this.cache.get(type);
+        if (!loader) {
+            loader = this.dataloaderFactory(this.id, type);
+            this.cache.set(type, loader);
+        }
+        return loader;
+    }
+}
+exports.NestDataLoaderContext = NestDataLoaderContext;
 let DataLoaderInterceptor = class DataLoaderInterceptor {
     constructor(moduleRef) {
         this.moduleRef = moduleRef;
@@ -37,24 +61,20 @@ let DataLoaderInterceptor = class DataLoaderInterceptor {
         }
         const ctx = graphql_1.GqlExecutionContext.create(context).getContext();
         if (ctx[NEST_LOADER_CONTEXT_KEY] === undefined) {
-            ctx[NEST_LOADER_CONTEXT_KEY] = {
-                contextId: core_1.ContextIdFactory.create(),
-                getLoader: (type) => {
-                    if (ctx[type] === undefined) {
-                        ctx[type] = (() => __awaiter(this, void 0, void 0, function* () {
-                            try {
-                                return (yield this.moduleRef.resolve(type, ctx[NEST_LOADER_CONTEXT_KEY].contextId, { strict: false })).generateDataLoader();
-                            }
-                            catch (e) {
-                                throw new common_1.InternalServerErrorException(`The loader ${type} is not provided` + e);
-                            }
-                        }))();
-                    }
-                    return ctx[type];
-                },
-            };
+            ctx[NEST_LOADER_CONTEXT_KEY] = new NestDataLoaderContext(this.createDataLoader.bind(this));
         }
         return next.handle();
+    }
+    createDataLoader(contextId, type) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const provider = yield this.moduleRef.resolve(type, contextId, { strict: false });
+                return provider.generateDataLoader();
+            }
+            catch (e) {
+                throw new common_1.InternalServerErrorException(`The loader ${type} is not provided` + e);
+            }
+        });
     }
 };
 DataLoaderInterceptor = __decorate([
@@ -62,18 +82,25 @@ DataLoaderInterceptor = __decorate([
     __metadata("design:paramtypes", [core_1.ModuleRef])
 ], DataLoaderInterceptor);
 exports.DataLoaderInterceptor = DataLoaderInterceptor;
+function getNestDataLoaderContext(context) {
+    if (context.getType() !== "graphql") {
+        throw new common_1.InternalServerErrorException("@Loader should only be used within the GraphQL context");
+    }
+    const graphqlContext = graphql_1.GqlExecutionContext.create(context).getContext();
+    const nestDataLoaderContext = graphqlContext[NEST_LOADER_CONTEXT_KEY];
+    if (!nestDataLoaderContext) {
+        throw new common_1.InternalServerErrorException(`You should provide interceptor ${DataLoaderInterceptor.name} globally with ${core_1.APP_INTERCEPTOR}`);
+    }
+    return nestDataLoaderContext;
+}
 exports.Loader = (0, common_1.createParamDecorator)((data, context) => {
     if (!data) {
         throw new common_1.InternalServerErrorException(`No loader provided to @Loader ('${data}')`);
     }
-    if (context.getType() !== "graphql") {
-        throw new common_1.InternalServerErrorException("@Loader should only be used within the GraphQL context");
-    }
-    const ctx = graphql_1.GqlExecutionContext.create(context).getContext();
-    if (!ctx[NEST_LOADER_CONTEXT_KEY]) {
-        throw new common_1.InternalServerErrorException(`You should provide interceptor ${DataLoaderInterceptor.name} globally with ${core_1.APP_INTERCEPTOR}`);
-    }
-    return ctx[NEST_LOADER_CONTEXT_KEY].getLoader(data);
+    return getNestDataLoaderContext(context).getLoader(data);
+});
+exports.LoaderContext = (0, common_1.createParamDecorator)((data, context) => {
+    return getNestDataLoaderContext(context);
 });
 const ensureOrder = (options) => {
     const { docs, keys, prop, error = (key) => `Document does not exist (${key})`, } = options;
